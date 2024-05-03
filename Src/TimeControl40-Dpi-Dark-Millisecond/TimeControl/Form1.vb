c9006037e12ea2fe7c855c21904eae6495ff2b27
cjhdevact <101956507+cjhdevact@users.noreply.github.com>
2024-05-03 15:04:04 +0800

    Upload 20240503

---
+++ Src/TimeControl40-Dpi-Dark-Millisecond/TimeControl/Form1.vb
@@ -0,0 +1,333 @@
+﻿'/*****************************************************\
+'*                                                     *
+'*     TimeControl - Form1.vb                          *
+'*                                                     *
+'*     Copyright (c) CJH.  All Rights Reserved.        *
+'*                                                     *
+'*     The main time form.                             *
+'*                                                     *
+'\*****************************************************/
+Imports Microsoft.Win32
+Imports System.Runtime.InteropServices
+
+Public Class Form1
+    <DllImport("dwmapi.dll")> _
+    Public Shared Function DwmSetWindowAttribute(ByVal hwnd As IntPtr, ByVal attr As DwmWindowAttribute, ByRef attrValue As Integer, ByVal attrSize As Integer) As Integer
+    End Function
+    'Public Shared Function EnableDarkModeForWindow(ByVal hWnd As IntPtr, ByVal enable As Boolean) As Boolean
+    '    Dim darkMode As Integer
+    '    darkMode = enable
+    '    Dim hr As Integer
+    '    Dim i As Integer
+    '    'hr = DwmSetWindowAttribute(hWnd, DwmWindowAttribute.UseImmersiveDarkMode, darkMode, sizeof(i))
+    '    Return hr >= 0
+    'End Function
+    Public Shared Function EnableDarkModeForWindow(ByVal hWnd As IntPtr, ByVal enable As Boolean) As Boolean
+        Dim attrValue As Integer = If(enable, 1, 0)
+        Return (Form1.DwmSetWindowAttribute(hWnd, DwmWindowAttribute.UseImmersiveDarkMode, attrValue, 4) >= 0)
+    End Function
+
+    Public Enum DwmWindowAttribute As UInt32
+        NCRenderingEnabled = 1
+        NCRenderingPolicy
+        TransitionsForceDisabled
+        AllowNCPaint
+        CaptionButtonBounds
+        NonClientRtlLayout
+        ForceIconicRepresentation
+        Flip3DPolicy
+        ExtendedFrameBounds
+        HasIconicBitmap
+        DisallowPeek
+        ExcludedFromPeek
+        Cloak
+        Cloaked
+        FreezeRepresentation
+        PassiveUpdateMode
+        UseHostBackdropBrush
+        UseImmersiveDarkMode = 20
+        WindowCornerPreference = 33
+        BorderColor
+        CaptionColor
+        TextColor
+        VisibleFrameBorderThickness
+        SystemBackdropType
+        Last
+    End Enum
+
+    Public Const DEVBRANCH = "TCTL_DEV_MILL"
+
+    Public a As New System.Drawing.Point
+    Public crmd As Integer ' 1=Dark 1=Light
+    Public appcolor As Integer ' 0= With System 1= Dark 2= Light
+    Public MovedV As Integer
+    Public UseMoveV As Integer
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Label1.Text = Format(Now(), "HH:mm:ss.") & DateTime.Now.Millisecond
+        If UseMoveV = 0 Then
+            If MovedV = 0 Then
+                If Me.Location <> a Then
+                    Me.Location = a
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        MovedV = 0
+        appcolor = 0
+        Dim disi As Graphics = Me.CreateGraphics()
+        Timer1.Enabled = True
+        'Me.Height = 38
+        'Me.Width = 120
+        If disi.DpiX <= 96 Then
+            Me.Height = 38
+            Me.Width = 174
+        Else
+            Me.Height = 38 * disi.DpiY * 0.01 * 1.15
+            Me.Width = 174 * disi.DpiX * 0.01 * 1.15
+        End If
+        'Me.Height = Label1.Height
+        'Me.Width = Label1.Width
+        a.X = (System.Windows.Forms.SystemInformation.PrimaryMonitorSize.Width - Me.Width) / 2
+        a.Y = 5 * disi.DpiY * 0.01
+        Me.Location = a
+        'ContextMenuStrip1.Font = New Font(ContextMenuStrip1.Font.Name, 8.25F * 96.0F / CreateGraphics().DpiX, ContextMenuStrip1.Font.Style, ContextMenuStrip1.Font.Unit, ContextMenuStrip1.Font.GdiCharSet, ContextMenuStrip1.Font.GdiVerticalFont)
+        'Font = New Font(Font.Name, 8.25F * 96.0F / CreateGraphics().DpiX, Font.Style, Font.Unit, Font.GdiCharSet, Font.GdiVerticalFont)
+
+        'Get Color Settings
+        Try
+            AddKey("Software\CJH", "HKCU")
+            AddKey("Software\CJH\TimeControl", "HKCU")
+            AddKey("Software\CJH\TimeControl\Settings", "HKCU")
+        Catch ex As Exception
+        End Try
+        Dim mykey As RegistryKey = Registry.CurrentUser.OpenSubKey("Software\CJH\TimeControl\Settings", True)
+        Dim myv As Integer
+        If (Not mykey Is Nothing) Then
+            'If (If((regkey.GetValue("") Is Nothing), Nothing, regkey.GetValue("").ToString) <> "AppsUseLightTheme") Then
+            'End If
+            myv = mykey.GetValue("ColorMode", -1)
+            If myv = 0 Then
+                appcolor = 0
+            ElseIf myv = -1 Then
+                appcolor = 0
+                AddReg("Software\CJH\TimeControl\Settings", "ColorMode", 0, Microsoft.Win32.RegistryValueKind.DWord, "HKCU")
+            ElseIf myv = 1 Then
+                appcolor = 1
+            ElseIf myv = 2 Then
+                appcolor = 2
+            End If
+        Else
+            appcolor = 0
+            AddReg("Software\CJH\TimeControl\Settings", "ColorMode", 0, Microsoft.Win32.RegistryValueKind.DWord, "HKCU")
+        End If
+
+
+        If appcolor = 0 Then
+            'Get System Color
+            Dim regkey As RegistryKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize", True)
+            Dim sysacr As Integer
+            Try
+                If (Not regkey Is Nothing) Then
+                    'If (If((regkey.GetValue("") Is Nothing), Nothing, regkey.GetValue("").ToString) <> "AppsUseLightTheme") Then
+                    'End If
+                    sysacr = regkey.GetValue("AppsUseLightTheme", -1)
+                    If sysacr = 0 Then
+                        crmd = 0
+                    ElseIf sysacr = 1 Then
+                        crmd = 1
+                    Else
+                        crmd = 1
+                    End If
+                Else
+                    crmd = 1
+                End If
+            Catch ex As Exception
+                crmd = 1
+            End Try
+            regkey.Close()
+        ElseIf appcolor = 1 Then
+            crmd = 1 'Light
+        ElseIf appcolor = 2 Then
+            crmd = 0 'Dark
+        End If
+        AddHandler SystemEvents.UserPreferenceChanged, AddressOf SystemEvents_UserPreferenceChanged
+
+        If (Not mykey Is Nothing) Then
+            Me.UseMoveV = mykey.GetValue("EnableDrag", -1)
+            If Me.UseMoveV = -1 Then
+                RegKeyModule.AddReg("Software\CJH\TimeControl\Settings", "EnableDrag", 1, RegistryValueKind.DWord, "HKCU")
+                Me.UseMoveV = 1
+            End If
+        Else
+            RegKeyModule.AddReg("Software\CJH\TimeControl\Settings", "EnableDrag", 1, RegistryValueKind.DWord, "HKCU")
+            Me.UseMoveV = 1
+        End If
+
+        Dim UseTop As Integer
+        If (Not mykey Is Nothing) Then
+            UseTop = mykey.GetValue("AllowTopMost", -1)
+            If UseTop = -1 Then
+                RegKeyModule.AddReg("Software\CJH\TimeControl\Settings", "AllowTopMost", 1, RegistryValueKind.DWord, "HKCU")
+                Me.TopMost = True
+            ElseIf UseTop = 0 Then
+                Me.TopMost = False
+            ElseIf UseTop = 1 Then
+                Me.TopMost = True
+            End If
+        Else
+            RegKeyModule.AddReg("Software\CJH\TimeControl\Settings", "AllowTopMost", 1, RegistryValueKind.DWord, "HKCU")
+            Me.TopMost = True
+        End If
+        mykey.Close()
+
+        Call formatcolorcur()
+        Call Form2.formatcolorcurset()
+    End Sub
+
+    'API移动窗体
+    Declare Function SendMessage Lib "user32" Alias "SendMessageA" (ByVal hwnd As IntPtr, ByVal wMsg As Integer, ByVal wParam As Integer, ByVal lParam As Integer) As Boolean
+    Declare Function ReleaseCapture Lib "user32" Alias "ReleaseCapture" () As Boolean
+    Const WM_SYSCOMMAND = &H112
+    Const SC_MOVE = &HF010&
+    Const HTCAPTION = 2
+    Private Sub Form1_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles MyBase.MouseDown
+        If UseMoveV = 1 Then
+            ReleaseCapture()
+            SendMessage(Me.Handle, WM_SYSCOMMAND, SC_MOVE + HTCAPTION, 0)
+            MovedV = 1
+        End If
+    End Sub
+    Private Sub Label1_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Label1.MouseDown
+        If UseMoveV = 1 Then
+            ReleaseCapture()
+            SendMessage(Me.Handle, WM_SYSCOMMAND, SC_MOVE + HTCAPTION, 0)
+            MovedV = 1
+        End If
+    End Sub
+
+    Sub formatcolorcur()
+        If crmd = 0 Then
+            Me.BackgroundImage = My.Resources.bkgdark
+            Me.Label1.ForeColor = Color.White
+            Me.ContextMenuStrip1.BackColor = Color.FromArgb(32, 32, 32)
+            Me.ContextMenuStrip1.ForeColor = Color.White
+            Me.BackColor = Color.FromArgb(32, 32, 32)
+            Me.ForeColor = Color.White
+            'Me.h10m.BackColor = Color.Black
+            'Me.h10m.ForeColor = Color.White
+            'Me.h1m.BackColor = Color.Black
+            'Me.h1m.ForeColor = Color.White
+            'Me.h30s.BackColor = Color.Black
+            'Me.h30s.ForeColor = Color.White
+            'Me.h5m.BackColor = Color.Black
+            'Me.h5m.ForeColor = Color.White
+            EnableDarkModeForWindow(Me.Handle, True)
+            Me.TransparencyKey = Color.FromArgb(1, 1, 1)
+        Else
+            Me.BackColor = Color.White
+            Me.ForeColor = Color.Black
+            Me.BackgroundImage = My.Resources.bkg
+            Me.Label1.ForeColor = Color.Black
+            Me.ContextMenuStrip1.BackColor = Color.White
+            Me.ContextMenuStrip1.ForeColor = Color.Black
+            EnableDarkModeForWindow(Me.Handle, False)
+            Me.TransparencyKey = Color.FromArgb(184, 184, 184)
+        End If
+    End Sub
+    Private Sub h30s_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles h30s.Click
+        Timer2.Interval = 30000
+        Me.Hide()
+        Timer2.Enabled = True
+    End Sub
+
+    Private Sub h1m_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles h1m.Click
+        Timer2.Interval = 60000
+        Me.Hide()
+        Timer2.Enabled = True
+    End Sub
+
+    Private Sub h5m_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles h5m.Click
+        Timer2.Interval = 300000
+        Me.Hide()
+        Timer2.Enabled = True
+    End Sub
+
+    Private Sub h10m_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles h10m.Click
+        Timer2.Interval = 600000
+        Me.Hide()
+        Timer2.Enabled = True
+    End Sub
+
+    Private Sub Timer2_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer2.Tick
+        Me.Show()
+        Timer2.Enabled = False
+    End Sub
+
+    Private Sub ext_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ext.Click
+        'If MessageBox.Show("确定要关闭时钟吗？", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) = MsgBoxResult.Yes Then
+        'End
+        'End If
+        Form2.ShowDialog()
+    End Sub
+
+    Private Sub Me_FormClosing(ByVal sender As Object, ByVal e As FormClosingEventArgs) Handles Me.FormClosing
+        'e.Cancel = True
+        Select Case (e.CloseReason)
+            '应用程序要求关闭窗口
+            Case CloseReason.ApplicationExitCall
+                e.Cancel = False '不拦截，响应操作
+                '自身窗口上的关闭按钮
+            Case CloseReason.FormOwnerClosing
+                e.Cancel = True '拦截，不响应操作
+                'MDI窗体关闭事件
+            Case CloseReason.MdiFormClosing
+                e.Cancel = True '拦截，不响应操作
+                '不明原因的关闭
+            Case CloseReason.None
+                e.Cancel = False
+                '任务管理器关闭进程
+            Case CloseReason.TaskManagerClosing
+                e.Cancel = False  '不拦截，响应操作
+                '用户通过UI关闭窗口或者通过Alt+F4关闭窗口
+            Case CloseReason.UserClosing
+                e.Cancel = True '拦截，不响应操作
+                '操作系统准备关机()
+            Case (CloseReason.WindowsShutDown)
+                e.Cancel = False '不拦截，响应操作
+        End Select
+
+    End Sub
+
+    'Private Sub Me_FormClosed(ByVal sender As Object, ByVal e As FormClosedEventArgs) Handles Me.FormClosed
+    'Me.Dispose()
+    'End Sub
+
+    Private Sub SystemEvents_UserPreferenceChanged(ByVal sender As Object, ByVal e As UserPreferenceChangedEventArgs)
+        If e.Category = UserPreferenceCategory.General Then
+            If appcolor = 0 Then
+                'Get System Color
+                Dim regkey As RegistryKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize", True)
+                Dim sysacr As Integer
+                If (Not regkey Is Nothing) Then
+                    'If (If((regkey.GetValue("") Is Nothing), Nothing, regkey.GetValue("").ToString) <> "AppsUseLightTheme") Then
+                    'End If
+                    sysacr = regkey.GetValue("AppsUseLightTheme", -1)
+                    If sysacr = 0 Then
+                        crmd = 0
+                    ElseIf sysacr = 1 Then
+                        crmd = 1
+                    Else
+                        crmd = 1
+                    End If
+                Else
+                    crmd = 1
+                End If
+                regkey.Close()
+                Call formatcolorcur()
+                Call Form2.formatcolorcurset()
+            End If
+        End If
+    End Sub
+End Class
---
+++ Src/TimeControl40-Dpi/TimeControl/Form2.Designer.vb
@@ -0,0 +1,223 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form2))
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button2
+        '
+        Me.Button2.ForeColor = System.Drawing.Color.Black
+        Me.Button2.Location = New System.Drawing.Point(345, 178)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(110, 27)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "应用"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Button1.ForeColor = System.Drawing.Color.Black
+        Me.Button1.Location = New System.Drawing.Point(345, 375)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(110, 27)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "关闭"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.BackColor = System.Drawing.Color.White
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.ComboBox1.ForeColor = System.Drawing.Color.Black
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"秒", "分钟", "小时"})
+        Me.ComboBox1.Location = New System.Drawing.Point(209, 177)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 28)
+        Me.ComboBox1.TabIndex = 6
+        '
+        'TextBox1
+        '
+        Me.TextBox1.ForeColor = System.Drawing.Color.Black
+        Me.TextBox1.Location = New System.Drawing.Point(83, 178)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(116, 27)
+        Me.TextBox1.TabIndex = 7
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(37, 276)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(81, 20)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "(Versions)"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("微软雅黑", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label2.Location = New System.Drawing.Point(18, 20)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(167, 30)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "设置时钟小工具"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(27, 70)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(69, 20)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "界面设置"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Checked = True
+        Me.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox1.Location = New System.Drawing.Point(41, 94)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(88, 24)
+        Me.CheckBox1.TabIndex = 11
+        Me.CheckBox1.Text = "始终顶置"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Checked = True
+        Me.CheckBox2.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox2.Location = New System.Drawing.Point(41, 124)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(88, 24)
+        Me.CheckBox2.TabIndex = 12
+        Me.CheckBox2.Text = "允许移动"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(27, 155)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(39, 20)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "关闭"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(37, 181)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(39, 20)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "隐藏"
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.DarkRed
+        Me.Button3.ForeColor = System.Drawing.Color.White
+        Me.Button3.Location = New System.Drawing.Point(41, 213)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(110, 27)
+        Me.Button3.TabIndex = 15
+        Me.Button3.Text = "退出"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(27, 249)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(39, 20)
+        Me.Label6.TabIndex = 16
+        Me.Label6.Text = "关于"
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.BackColor = System.Drawing.Color.White
+        Me.CancelButton = Me.Button1
+        Me.ClientSize = New System.Drawing.Size(467, 415)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button2)
+        Me.Font = New System.Drawing.Font("微软雅黑", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.ForeColor = System.Drawing.Color.Black
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.MaximizeBox = False
+        Me.Name = "Form2"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "设置时钟小工具"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox2 As System.Windows.Forms.CheckBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+End Class
